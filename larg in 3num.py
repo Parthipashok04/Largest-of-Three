afc27a7eda3987f3dc7d26ce96cb6b9c0f47e672
Parthip Ashok <parthipashok04@gmail.com>
2025-02-23 22:23:37 +0530

    Add files via upload

---
+++ larg in 3num.py
@@ -0,0 +1,9 @@
+a=int(input('Enter the First Number:\t'))
+b=int(input('Enter the Second Number:\t'))
+c=int(input('Enter the Third Number:\t'))
+if a>b and b>c:
+   print(a)
+elif b>c and b>a:
+   print(b)
+else:
+   print(c)
